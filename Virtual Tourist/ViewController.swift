fe8ecef495f1daaa035f3fa5524cb7e0d414d6cd
Malik Basalamah <basalamahmalik@gmail.com>
2018-12-19 15:41:22 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Malik Basalamah on 12/04/1440 AH.
+//  Copyright © 1440 Malik Basalamah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
